eb4e11ed6c21bffa335ee9f10aac1e3ccef30211
Pio <p990301@gmail.com>
2021-12-26 21:21:47 +0900

    Changes

--- DatabaseExample/app8.js
+++ DatabaseExample/app8.js
@@ -8,25 +8,86 @@ var bodyParser = require('body-parser')
     , errorHandler = require('errorhandler');
 
 var expressErrorHandler = require('express-error-handler');
+
 var expressSession = require('express-session');
+
 var mysql = require('mysql');
-var pool = mysql.createPool({
+
+var pool      =    mysql.createPool({
     connectionLimit : 10,
-    host : 'localhost',
-    user : 'hello', password : '12345', database : 'test', debug : false
+    host     : 'localhost',
+    user     : 'hello',
+    password : '12345',
+    database : 'test',
+    debug    :  false
 });
+
 var app = express();
-var router = express.Router();
+
 app.set('port', process.env.PORT || 3000);
+
 app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use(bodyParser.json())
+
 app.use('/public', static(path.join(__dirname, 'public')));
+
 app.use(cookieParser());
+
 app.use(expressSession({
     secret:'my key',
     resave:true,
     saveUninitialized:true
 }));
+
+var router = express.Router();
+
+router.route('/process/login').post(function(req, res) {
+    console.log('/process/login 호출됨.');
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+
+    console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword);
+
+    if (pool) {
+        authUser(paramId, paramPassword, function(err, rows) {
+            if (err) {
+                console.error('사용자 로그인 중 에러 발생 : ' + err.stack);
+
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+                res.write('<h2>사용자 로그인 중 에러 발생</h2>');
+                res.write('<p>' + err.stack + '</p>');
+                res.end();
+
+                return;
+            }
+            if (rows) {
+                console.dir(rows);
+                var username = rows[0].name;
+
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+                res.write('<h1>로그인 성공</h1>');
+                res.write('<div><p>사용자 아이디 : ' + paramId + '</p></div>');
+                res.write('<div><p>사용자 이름 : ' + username + '</p></div>');
+                res.write("<br><br><a href='/public/login2.html'>다시 로그인하기</a>");
+                res.end();
+
+            } else {
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+                res.write('<h1>로그인  실패</h1>');
+                res.write('<div><p>아이디와 패스워드를 다시 확인하십시오.</p></div>');
+                res.write("<br><br><a href='/public/login2.html'>다시 로그인하기</a>");
+                res.end();
+            }
+        });
+    } else {
+        res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+        res.write('<h2>데이터베이스 연결 실패</h2>');
+        res.write('<div><p>데이터베이스에 연결하지 못했습니다.</p></div>');
+        res.end();
+    }
+
+});
 
 router.route('/process/adduser').post(function(req, res) {
     console.log('/process/adduser 호출됨.');
@@ -50,15 +111,8 @@ router.route('/process/adduser').post(function(req, res) {
 
                 return;
             }
-
             if (addedUser) {
                 console.dir(addedUser);
-
-               /* console.log('inserted ' + result.affectedRows + ' rows');
-
-                var insertId = result.insertId;
-                console.log('추가한 레코드의 아이디 : ' + insertId);*/
-
                 res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
                 res.write('<h2>사용자 추가 성공</h2>');
                 res.end();
@@ -119,6 +173,7 @@ var authUser = function(id, password, callback) {
 
 var addUser = function(id, name, age, password, callback) {
     console.log('addUser 호출됨 : ' + id + ', ' + password + ', ' + name + ', ' + age);
+
     pool.getConnection(function(err, conn) {
         if (err) {
             if (conn) {
@@ -129,20 +184,26 @@ var addUser = function(id, name, age, password, callback) {
             return;
         }
         console.log('데이터베이스 연결 스레드 아이디 : ' + conn.threadId);
+
         var data = {id:id, name:name, age:age, password:password};
+
         var exec = conn.query('insert into users set ?', data, function(err, result) {
-            conn.release();  // 반드시 해제해야 함
+            conn.release();
             console.log('실행 대상 SQL : ' + exec.sql);
+
             if (err) {
                 console.log('SQL 실행 시 에러 발생함.');
                 console.dir(err);
+
                 callback(err, null);
+
                 return;
             }
 
             callback(null, result);
 
         });
+
         conn.on('error', function(err) {
             console.log('데이터베이스 연결 시 에러 발생함.');
             console.dir(err);
@@ -162,8 +223,15 @@ var errorHandler = expressErrorHandler({
 app.use( expressErrorHandler.httpError(404) );
 app.use( errorHandler );
 
+process.on('SIGTERM', function () {
+    console.log("프로세스가 종료됩니다.");
+});
 
+app.on('close', function () {
+    console.log("Express 서버 객체가 종료됩니다.");
+});
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('서버가 시작되었습니다. 포트 : ' + app.get('port'));
 });
+ 
