0ceb9110ac0d96f3a4fed533e5984c23c73ec5cd
pyojisung <p990301@gmail.com>
2022-01-19 11:56:52 +0900

    1/19 study passport

--- PassportExample/app.js
+++ PassportExample/app.js
@@ -1,152 +1,91 @@
-var express = require('express'),
-    http = require('http'),
-    path = require('path');
+// Express 기본 모듈 불러오기
+var express = require('express')
+    , http = require('http')
+    , path = require('path');
 
-var bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
-    static = require('serve-static'),
-    errorHandler = require('errorhandler');
+// Express의 미들웨어 불러오기
+var bodyParser = require('body-parser')
+    , cookieParser = require('cookie-parser')
+    , static = require('serve-static')
+    , errorHandler = require('errorhandler');
 
+// 에러 핸들러 모듈 사용
 var expressErrorHandler = require('express-error-handler');
 
-var config = require('./config');
+// Session 미들웨어 불러오기
 var expressSession = require('express-session');
-var route_loader = require('./routes/route_loader');
-var database = require('./database/database');
 
+
+//===== Passport 사용 =====//
 var passport = require('passport');
 var flash = require('connect-flash');
 
+
+// 모듈로 분리한 설정 파일 불러오기
+var config = require('./config');
+
+// 모듈로 분리한 데이터베이스 파일 불러오기
+var database = require('./database/database');
+
+// 모듈로 분리한 라우팅 파일 불러오기
+var route_loader = require('./routes/route_loader');
+
+
+
+
+// 익스프레스 객체 생성
 var app = express();
+
+
+//===== 뷰 엔진 설정 =====//
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+console.log('뷰 엔진이 ejs로 설정되었습니다.');
+
+
+//===== 서버 변수 설정 및 static으로 public 폴더 설정  =====//
+console.log('config.server_port : %d', config.server_port);
+app.set('port', process.env.PORT || 3000);
+
+
+// body-parser를 이용해 application/x-www-form-urlencoded 파싱
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// body-parser를 이용해 application/json 파싱
+app.use(bodyParser.json())
+
+// public 폴더를 static으로 오픈
+app.use('/public', static(path.join(__dirname, 'public')));
+
+// cookie-parser 설정
+app.use(cookieParser());
+
+// 세션 설정
+app.use(expressSession({
+    secret:'my key',
+    resave:true,
+    saveUninitialized:true
+}));
+
+
+
+//===== Passport 사용 설정 =====//
+// Passport의 세션을 사용할 때는 그 전에 Express의 세션을 사용하는 코드가 있어야 함
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-console.log('뷰 엔진이 ejs로 설정');
 
-console.log('config.server_port : %d', config.server_port);
-app.set('port', process.env.PORT || 3000);
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.use('/public', static(path.join(__dirname, 'public')));
-app.use(cookieParser());
-
-app.use(expressSession({
-    secret: 'my key',
-    resave: true,
-    saveUninitialized: true
-}));
-
-route_loader.init(app, express.Router());
-
-var errorhandler = expressErrorHandler({
-    static: {
-        '404': './public/404.html'
-    }
-});
-
-app.use(expressErrorHandler.httpError(404));
-app.use(errorhandler);
-
-process.on('uncaughtException', function (err) {
-    console.log('uncaughtException' + err);
-    console.log('서버 프로세스 종료하지 않고 유지');
-    console.log(err.stack);
-});
-
-process.on('SIGTERM', function () {
-    console.log('프로세스 종료');
-    app.close();
-});
-
-app.on('close', function () {
-    console.log("express 서버 객체가 종료됩니다");
-    if (database.db) {
-        database.db.close();
-    }
-
-});
-
-var LocalStrategy = require('passport-local').Strategy;
-
-passport.use('local-login', new LocalStrategy({
-    usernameFiled: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-}, function (req, email, password, done) {
-    console.log('passport local-login 호출 : ' + email + ", " + password);
-
-    var database = app.get('database');
-    database.UserModel.findOne({'email': email}, function (err, user) {
-        if (err) {
-            return done(err);
-        }
-
-        if (!user) {
-            console.log('계정이 일치하지 않음');
-            return done(null, false, req.flash('loginMessage', '등록된 계정이 없습니다.'));
-
-        }
-        var authenticated = user.authenticate(password, user._doc.salt, user._doc.hashed_password);
-        if (!authenticated) {
-            console.log('비밀번호 일치하지 않음');
-            return done(null, false, req.flash('loginMessage', '비밀번호가 일치하지 않음'));
-        }
-        console.log('계정과 비밀번호가 일치');
-        return done(null, user);
-    });
-}));
-
-passport.use('local-signup', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-}, function (req, email, password, done) {
-    var paramName = req.body.name || req.query.name;
-    console.log('passport local-signup 호출 : ' + email + ", " + password + ", " + paramName);
-
-    process.nextTick(function () {
-        var database = app.get('database');
-        database.UserModel.findOne({'email': email}, function (err, user) {
-            if (err) {
-                return done(err);
-            }
-            if (user) {
-                console.log('기존 계정이 있음');
-                return done(null, false, req.flash('signupMessage', '계정이 이미 존재'));
-            } else {
-                var user = new database.UserModel({'email': email, 'password': password, 'name': paramName});
-
-                user.save(function (err) {
-                    if (err) {
-                        throw  err;
-                    }
-                    console.log("사용자 데이터 추가함");
-                    return done(null, user);
-                });
-            }
-        });
-    });
-}));
-passport.serializeUser(function (user,done){
-    console.log('serializeUser()호출됨.');
-    console.dir(user);
-    done(null,user);
-});
-passport.deserializeUser(function (user,done){
-    console.log('deserializeUser()호출');
-    console.dir(user);
-    done(null,user);
-});
-
+//라우팅 정보를 읽어 들여 라우팅 설정
 var router = express.Router();
+route_loader.init(app, router);
 
-var route_loader = require('./routes/route_loader');
-route_loader.init(app,router);
+
+
+
+//===== Passport 관련 라우팅 =====//
+
+// 홈 화면 - index.ejs 템플릿을 이용해 홈 화면이 보이도록 함
 router.route('/').get(function(req, res) {
     console.log('/ 패스 요청됨.');
     res.render('index.ejs');
@@ -215,11 +154,151 @@ router.route('/logout').get(function(req, res) {
 });
 
 
-var server = http.createServer(app).listen(app.get('port'), function () {
-    console.log('서버 시작 포트 : ' + app.get('port'));
 
-    database.init(app, config);
+//===== Passport Strategy 설정 =====//
+
+var LocalStrategy = require('passport-local').Strategy;
+
+//패스포트 로그인 설정
+passport.use('local-login', new LocalStrategy({
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true   // 이 옵션을 설정하면 아래 콜백 함수의 첫번째 파라미터로 req 객체 전달됨
+}, function(req, email, password, done) {
+    console.log('passport의 local-login 호출됨 : ' + email + ', ' + password);
+
+    var database = app.get('database');
+    database.UserModel.findOne({ 'email' :  email }, function(err, user) {
+        if (err) { return done(err); }
+
+        // 등록된 사용자가 없는 경우
+        if (!user) {
+            console.log('계정이 일치하지 않음.');
+            return done(null, false, req.flash('loginMessage', '등록된 계정이 없습니다.'));  // 검증 콜백에서 두 번째 파라미터의 값을 false로 하여 인증 실패한 것으로 처리
+        }
+
+        // 비밀번호 비교하여 맞지 않는 경우
+        var authenticated = user.authenticate(password, user._doc.salt, user._doc.hashed_password);
+        if (!authenticated) {
+            console.log('비밀번호 일치하지 않음.');
+            return done(null, false, req.flash('loginMessage', '비밀번호가 일치하지 않습니다.'));  // 검증 콜백에서 두 번째 파라미터의 값을 false로 하여 인증 실패한 것으로 처리
+        }
+
+        // 정상인 경우
+        console.log('계정과 비밀번호가 일치함.');
+        return done(null, user);  // 검증 콜백에서 두 번째 파라미터의 값을 user 객체로 넣어 인증 성공한 것으로 처리
+    });
+
+}));
+
+
+// 패스포트 회원가입 설정
+passport.use('local-signup', new LocalStrategy({
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true    // 이 옵션을 설정하면 아래 콜백 함수의 첫번째 파라미터로 req 객체 전달됨
+}, function(req, email, password, done) {
+    // 요청 파라미터 중 name 파라미터 확인
+    var paramName = req.body.name || req.query.name;
+
+    console.log('passport의 local-signup 호출됨 : ' + email + ', ' + password + ', ' + paramName);
+
+    // findOne 메소드가 blocking되지 않도록 하고 싶은 경우, async 방식으로 변경
+    process.nextTick(function() {
+        var database = app.get('database');
+        database.UserModel.findOne({ 'email' :  email }, function(err, user) {
+            // 에러 발생 시
+            if (err) {
+                return done(err);
+            }
+
+            // 기존에 사용자 정보가 있는 경우
+            if (user) {
+                console.log('기존에 계정이 있음.');
+                return done(null, false, req.flash('signupMessage', '계정이 이미 있습니다.'));  // 검증 콜백에서 두 번째 파라미터의 값을 false로 하여 인증 실패한 것으로 처리
+            } else {
+                // 모델 인스턴스 객체 만들어 저장
+                var user = new database.UserModel({'email':email, 'password':password, 'name':paramName});
+                user.save(function(err) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    console.log("사용자 데이터 추가함.");
+                    return done(null, user);  // 검증 콜백에서 두 번째 파라미터의 값을 user 객체로 넣어 인증 성공한 것으로 처리
+                });
+            }
+        });
+    });
+
+}));
+
+
+
+// 사용자 인증 성공 시 호출
+// 사용자 정보를 이용해 세션을 만듦
+// 로그인 이후에 들어오는 요청은 deserializeUser 메소드 안에서 이 세션을 확인할 수 있음
+passport.serializeUser(function(user, done) {
+    console.log('serializeUser() 호출됨.');
+    console.dir(user);
+
+    done(null, user);  // 이 인증 콜백에서 넘겨주는 user 객체의 정보를 이용해 세션 생성
+});
+
+// 사용자 인증 이후 사용자 요청 시마다 호출
+// user -> 사용자 인증 성공 시 serializeUser 메소드를 이용해 만들었던 세션 정보가 파라미터로 넘어온 것임
+passport.deserializeUser(function(user, done) {
+    console.log('deserializeUser() 호출됨.');
+    console.dir(user);
+
+    // 사용자 정보 중 id나 email만 있는 경우 사용자 정보 조회 필요 - 여기에서는 user 객체 전체를 패스포트에서 관리
+    // 두 번째 파라미터로 지정한 사용자 정보는 req.user 객체로 복원됨
+    // 여기에서는 파라미터로 받은 user를 별도로 처리하지 않고 그대로 넘겨줌
+    done(null, user);
 });
 
 
 
+
+//===== 404 에러 페이지 처리 =====//
+var errorHandler = expressErrorHandler({
+    static: {
+        '404': './public/404.html'
+    }
+});
+
+app.use( expressErrorHandler.httpError(404) );
+app.use( errorHandler );
+
+
+//===== 서버 시작 =====//
+
+//확인되지 않은 예외 처리 - 서버 프로세스 종료하지 않고 유지함
+process.on('uncaughtException', function (err) {
+    console.log('uncaughtException 발생함 : ' + err);
+    console.log('서버 프로세스 종료하지 않고 유지함.');
+
+    console.log(err.stack);
+});
+
+// 프로세스 종료 시에 데이터베이스 연결 해제
+process.on('SIGTERM', function () {
+    console.log("프로세스가 종료됩니다.");
+    app.close();
+});
+
+app.on('close', function () {
+    console.log("Express 서버 객체가 종료됩니다.");
+    if (database.db) {
+        database.db.close();
+    }
+});
+
+// 시작된 서버 객체를 리턴받도록 합니다.
+var server = http.createServer(app).listen(app.get('port'), function(){
+    console.log('서버가 시작되었습니다. 포트 : ' + app.get('port'));
+
+    // 데이터베이스 초기화
+    database.init(app, config);
+
+});
