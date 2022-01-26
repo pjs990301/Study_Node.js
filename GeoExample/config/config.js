4f8e33334ccb4f5bccbf7f41366be0618b4a2c4c
pyojisung <p990301@gmail.com>
2022-01-26 09:56:09 +0900

    1/26 study 위치기반서비스

---
+++ GeoExample/config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	server_port: 3000,
+	db_url: 'mongodb://localhost:27017/local',
+	db_schemas: [
+	    {file:'./user_schema', collection:'users6', schemaName:'UserSchema', modelName:'UserModel'}
+	],
+	route_info: [
+	],
+	facebook: {		// passport facebook
+		clientID: 'id',
+		clientSecret: 'pw',
+		callbackURL: 'http://localhost:3000/auth/facebook/callback'
+	},
+	twitter: {		// passport twitter
+		clientID: 'id',
+		clientSecret: 'secret',
+		callbackURL: '/auth/twitter/callback'
+	},
+	google: {		// passport google
+		clientID: 'id',
+		clientSecret: 'secret',
+		callbackURL: '/auth/google/callback'
+	}
+}
