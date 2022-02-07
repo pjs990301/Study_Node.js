a8887da988d543355d0f16d8cee23f786c8ed1fa
pyojisung <p990301@gmail.com>
2022-02-07 09:51:23 +0900

    2/7 study 위치기반서비스 schema

---
+++ GeoExample/database/coffeeshop_schema.js
@@ -0,0 +1,22 @@
+var Schema = { };
+
+Schema.createSchema = function (mongoose){
+    var CoffeeShopSchema = mongoose.set({
+        name : {type : String, index : 'hashed', 'default' : ''},
+        address : {type : String, 'default' : ''},
+        tel : { type : String, 'default' : ''},
+        geometry : {
+            'type' : {type : String, 'default' : ''},
+            coordinates : [{type : "Number"}]
+        },
+        created_at : {type : Date, index : {unique :false}, 'default' : Date.now},
+        updated_at : {type : Date, index : {unique :false}, 'default' : Date.now}
+    });
+
+    CoffeeShopSchema.index({geometry : '2dsphere'});
+
+    CoffeeShopSchema.static('findAll',function(callback){
+        return this.find({},callback);
+    });
+
+}
