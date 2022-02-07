15fd4410e65dba4f5a0fbc201758c84cf4f4471e
pyojisung <p990301@gmail.com>
2022-02-07 10:03:01 +0900

    2/7 study 위치기반서비스 config 설정

--- GeoExample/config/config.js
+++ GeoExample/config/config.js
@@ -1,24 +1,35 @@
 module.exports = {
-	server_port: 3000,
-	db_url: 'mongodb://localhost:27017/local',
-	db_schemas: [
-	    {file:'./user_schema', collection:'users6', schemaName:'UserSchema', modelName:'UserModel'}
-	],
-	route_info: [
-	],
-	facebook: {		// passport facebook
-		clientID: 'id',
-		clientSecret: 'pw',
-		callbackURL: 'http://localhost:3000/auth/facebook/callback'
-	},
-	twitter: {		// passport twitter
-		clientID: 'id',
-		clientSecret: 'secret',
-		callbackURL: '/auth/twitter/callback'
-	},
-	google: {		// passport google
-		clientID: 'id',
-		clientSecret: 'secret',
-		callbackURL: '/auth/google/callback'
-	}
+    server_port: 3000,
+    db_url: 'mongodb://localhost:27017/local',
+    db_schemas: [
+        {
+            file: './user_schema',
+            collection: 'users6',
+            schemaName: 'UserSchema',
+            modelName: 'UserModel'
+        },
+        {
+            file: './coffeeshop_schema',
+            collection: 'coffeeshop',
+            schemaName: 'CoffeeShopSchema',
+            modelName: 'CoffeeShopMdodel'
+        }
+
+    ],
+    route_info: [],
+    facebook: {		// passport facebook
+        clientID: 'id',
+        clientSecret: 'pw',
+        callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    },
+    twitter: {		// passport twitter
+        clientID: 'id',
+        clientSecret: 'secret',
+        callbackURL: '/auth/twitter/callback'
+    },
+    google: {		// passport google
+        clientID: 'id',
+        clientSecret: 'secret',
+        callbackURL: '/auth/google/callback'
+    }
 }
